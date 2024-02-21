0c9231474d15b635a93d2348bd64e554b7f37f72
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2024-02-21 15:23:22 +0100

    Finished burgermenu with props for color change

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,33 @@
+import Navigation from "../components/Navigation";
+import { HiMenuAlt3, HiX } from "react-icons/hi"; // Import the HiX icon
+import React, { useState } from "react";
+
+const BurgerMenu = ({ menuIconColor }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="relative">
+      {!isOpen ? (
+        <HiMenuAlt3
+          className="text-3xl cursor-pointer z-50"
+          style={{ color: menuIconColor }}
+          onClick={toggleMenu}
+        />
+      ) : (
+        <div className="fixed inset-0 bg-white z-40">
+          <HiX
+            className="text-3xl cursor-pointer text-gray-500 z-50 absolute top-10 right-10"
+            onClick={toggleMenu}
+          />
+          <Navigation onClose={toggleMenu} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BurgerMenu;
